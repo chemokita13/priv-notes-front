32421e3548fc53584b357cb873670a25be86adb7
chemokita13 <chemokita13@gmail.com>
2023-02-04 23:12:19 +0100

    login page finished

--- src/pages/users/login.tsx
+++ src/pages/users/login.tsx
@@ -1,7 +1,82 @@
-import React, { Component } from "react";
+import axios from "axios";
 
-export default class Login extends Component {
+import React, { Component, ReactPropTypes } from "react";
+import { NextRouter, withRouter } from "next/router";
+
+interface MyState {
+    user: {
+        username: string;
+        password: string;
+    };
+}
+interface WithRouterProps {
+    router: NextRouter;
+}
+
+interface MyProps extends WithRouterProps {}
+class Login extends Component<MyProps, MyState> {
+    constructor(props: MyProps) {
+        super(props);
+        this.state = {
+            user: {
+                username: "",
+                password: "",
+            },
+        };
+    }
+
+    // It runs when an input is changed, setting user state
+    handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        this.setState((prevstate) => ({
+            user: {
+                ...prevstate.user,
+                [name]: value,
+            },
+        }));
+    };
+
+    handleSubmitForm = async (event: React.FormEvent<HTMLFormElement>) => {
+        // Prevents the page from reloading when the form is submitted
+        event.preventDefault();
+        try {
+            // Sends the user state to the server
+            const { data } = await axios.post(
+                "http://localhost:4000/auth",
+                this.state.user
+            );
+            // set the auth token in cookies
+            document.cookie = `token=${data.access_token}`;
+            // redirect to home notes page
+            this.props.router.push("/notes");
+        } catch (error) {
+            alert("Something went wrond, try again");
+        }
+    };
+
     render() {
-        return <div>login</div>;
+        return (
+            <div>
+                <form action="" onSubmit={(e) => this.handleSubmitForm(e)}>
+                    <input
+                        type="text"
+                        name="username"
+                        id="name"
+                        placeholder="username"
+                        onChange={(e) => this.handleInputChange(e)}
+                    />
+                    <input
+                        type="text"
+                        name="password"
+                        id="pass"
+                        placeholder="password"
+                        onChange={(e) => this.handleInputChange(e)}
+                    />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        );
     }
 }
+
+export default withRouter(Login);
