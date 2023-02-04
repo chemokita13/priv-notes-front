8b232bf202b523a108c62dc4fda4d0e709efc5c8
chemokita13 <chemokita13@gmail.com>
2023-02-04 12:57:33 +0100

    basic pages and routes structure added

---
+++ src/pages/notes/new.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class NewNote extends Component {
+    render() {
+        return <div>NewNote</div>;
+    }
+}
---
+++ src/pages/notes/update.tsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class Update extends Component {
+    render() {
+        return <div>Update</div>;
+    }
+}
