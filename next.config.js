ee25ea148bcd715dc6c4646d4bbb13e0a70b8d53
chemokita13 <chemokita13@gmail.com>
2023-03-07 19:37:49 +0100

    Added to ENV the API route

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env:{
+    API: process.env.API || 'http://localhost:4000',
+  }
 }
 
 module.exports = nextConfig
--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,1 +1,1 @@
-export const API = 'http://localhost:4000';
+export const API = process.env.API;
