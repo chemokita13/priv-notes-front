9f1fb84c7913e83cce8fe712f119164e8a087484
chemokita13 <chemokita13@gmail.com>
2023-03-08 20:47:43 +0100

    new user page finished

--- src/pages/users/new.tsx
+++ src/pages/users/new.tsx
@@ -1,7 +1,62 @@
-import React, { Component } from "react";
+import { API } from "@/constants/constant";
+import { User } from "@/dataTypes/user";
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
-export default class NewUser extends Component {
-    render() {
-        return <div>User</div>;
-    }
+function newUser() {
+    const router = useRouter(); // This is the hook to use next router
+
+    // User state to create and send to server
+    const [NewUser, setNewUser] = useState<User>({
+        username: "",
+        password: "",
+    });
+
+    const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement>
+    ): void => {
+        const { name, value } = event.target;
+        setNewUser({ ...NewUser, [name]: value });
+    };
+
+    const handleSubmitForm = async (
+        event: React.FormEvent<HTMLFormElement>
+    ): Promise<void> => {
+        event.preventDefault(); // Prevents page refresh
+        try {
+            const response = await axios.post(`${API}/users`, NewUser, {
+                withCredentials: true,
+            });
+            alert("User created successfully! Now, you can login");
+            router.push("/users/login");
+        } catch (error) {
+            alert("Something went wrong, try again");
+            router.push("/");
+        }
+    };
+
+    return (
+        <div>
+            <form onSubmit={(e) => handleSubmitForm(e)}>
+                <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    placeholder="Username: "
+                    onChange={(e) => handleInputChange(e)}
+                />
+                <input
+                    type="text"
+                    name="password"
+                    id="password"
+                    placeholder="Password: "
+                    onChange={(e) => handleInputChange(e)}
+                />
+                <button type="submit">Create User</button>
+            </form>
+        </div>
+    );
 }
+
+export default newUser;
