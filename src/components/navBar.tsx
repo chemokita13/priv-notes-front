522a96dd089bcced7ca4f9ebf80b3d10cdd50c6a
chemokita13 <chemokita13@gmail.com>
2023-03-09 22:08:01 +0100

    navbar checkpoint

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Cookies from "universal-cookie";
 
 function NavBar() {
-    return <nav>NavBar</nav>;
+    const cookie = new Cookies(); // cookie object to access cookies
+    const [Token, setToken] = useState(false);
+
+    useEffect(() => {
+        const token = cookie.get("access_token"); // get the token cookie
+        console.log(token);
+        if (token) {
+            setToken(true);
+        } else {
+            setToken(false);
+        }
+    }, []);
+
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+
+                {Token ? (
+                    <ul>
+                        <li>Login</li>
+                        <li>Register</li>
+                    </ul>
+                ) : (
+                    <ul>
+                        <li>Logout</li>
+                        <li> Notes</li>
+                    </ul>
+                )}
+            </ul>
+        </nav>
+    );
 }
 
 export default NavBar;
