50a7b264332230aa6f6b81bf5da8cf69de5099de
chemokita13 <chemokita13@gmail.com>
2023-03-21 21:19:37 +0100

    footer styles

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
     return (
-        <footer className="sticky bottom-0 flex flex-col w-full p-3 text-center shadow-lg md:p-5 md:justify-between md:flex-row bg-cyan-700">
+        <footer className="sticky bottom-0 flex flex-col w-full p-3 text-center shadow-lg md:p-5 md:justify-between md:flex-row bg-cyan-700 ">
             <button className="p-0 m-1 font-medium transition-all duration-200 border rounded-lg border-cyan-600 md:p-5 bg-emerald-500 hover:scale-110 hover:drop-shadow-lg md:drop-shadow-md hover:border-cyan-400">
                 <a href="https://github.com/chemokita13/priv-notes-front">
                     View source code on github
